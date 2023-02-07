506ffc64fb4b7d7eea84a25027d4a861d9da42aa
minseo-hong <77859988+minseo-hong@users.noreply.github.com>
2023-02-07 20:35:24 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DIAL_STR_MAX_SIZE   20
+
+int main(void) {
+    char dialStr[DIAL_STR_MAX_SIZE];
+    scanf("%s", dialStr);
+    
+    int totalDialTime = 0, dialTime;
+    for (int i = 0; i < strlen(dialStr); i++) {
+        dialTime = 3;
+        for (int j = 'A'; j <= 'O'; j += 3) {
+            if (dialStr[i] >= j && dialStr[i] <= j + 2)
+                break;
+            dialTime++;
+        }
+        if (dialStr[i] >= 'P' && dialStr[i] <= 'S') {
+            dialTime = 8;
+        } else if (dialStr[i] >= 'S' && dialStr[i] <= 'V') {
+            dialTime = 9;
+        } else if (dialStr[i] >= 'W' && dialStr[i] <= 'Z') {
+            dialTime = 10;
+        }
+        totalDialTime += dialTime;
+    }
+    
+    printf("%d\n", totalDialTime);
+    
+    return 0;
+}
